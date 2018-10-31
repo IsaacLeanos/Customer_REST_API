4a27d3b49fca906d22b2adf5419dd312314bc077
unknown <isaac.leanos11@gmail.com>
2018-10-31 05:14:49 -0700

    Customer schema

---
+++ index.js
@@ -0,0 +1,28 @@
+const restify=require('restify');
+const mongoose=require('mongoose');
+const config=require('./config');
+
+const server=restify.createServer();
+
+// middleware
+server.use(restify.plugins.bodyParser());
+
+// server
+server.listen(config.PORT,()=>{
+    mongoose.connect(config.MONGODB_URI,{useNewUrlParser:true});
+    console.log('Server up up and away!');
+});
+
+// mongo database connection
+const db=mongoose.connection;
+
+db.on('error',(e)=>{
+    console.log('database error',e);
+});
+
+//successful mongo connection
+db.once('open',()=>{
+    // ?
+    require('./route/customers')(server);
+    console.log(`Mongo up up and away on port ${config.PORT}!`);
+});
---
+++ model/Customer.js
@@ -0,0 +1,4 @@
+const mongoose=require('mongoose');
+const timestamp=require('mongoose-timestamp');
+
+const CustomerSchema=new mongoose.Schema({});
